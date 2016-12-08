eb87e00b58ca1ac1cd381399d5ef0c40d0c6bb9d
Young Ju Park <parkyngj@gmail.com>
2016-12-07 22:14:28 -0800

    sets up server js file with express and handlebars

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var app = express();
+var hbs = exphbs.create({
+  extname: '.hbs'
+});
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+// var util = require('./util.js')
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static('static'));
+
+app.get('/', function(req,res){
+  res.render('game');
+});
+
+http.listen(app.get('port'));
