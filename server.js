1849c59487c7b3cb792e956a02f1275e32fc915b
Young Ju Park <parkyngj@gmail.com>
2016-12-21 17:32:40 -0800

    add correct mongoose connection to mongodb code in server js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ require('dotenv').config();
 var mongoose = require('mongoose');
 
 // Mongoose connection to MongoDB
-db = mongoose.connect('mongodb://sally:' + process.env.MLAB_DB_PASSWORD + ":41358/" + process.env.MLAB_DB_NAME, function(err){
+db = mongoose.connect('mongodb://sally:' + process.env.MLAB_DB_PASSWORD + '@ds141358.mlab.com:41358/' + process.env.MLAB_DB_NAME, function(err){
   if(err){
     console.log(err);
   }
